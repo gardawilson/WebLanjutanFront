d9c0bfb4179977595e90fd8626a4761b99c444cd
Gravelazer <gardawilson@gmail.com>
2021-05-27 22:45:15 +0700

    Commit pertama

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div>TodoList</div>
+        <ul>
+            <li v-for="item in todos">{{item.desc}}</li>
+        </ul>
+        <input v-model="myText"/>
+        <button @click="tambahkan">Klik Me</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            todos:[],
+            myText: ''
+        }
+    },
+    methods: {
+     tambahkan: function(){
+         this.todos.push({desc: this.myText})
+     }
+    }
+}
+</script>
+
+
